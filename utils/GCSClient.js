ee48d9fdb933c62f191df06ab5cffb41f400403e
Mohdniyam <niyamul.haque98@gmail.com>
2025-10-13 19:45:22 +0530

    "minor change"

--- utils/GCSClient.js
+++ utils/GCSClient.js
@@ -1,10 +1,10 @@
 require("dotenv").config();
 const { Storage } = require("@google-cloud/storage");
-const dotenv = require("dotenv");
+const path = require("path");
 // Create a new Google Cloud Storage client
 const storage = new Storage({
   projectId: 'sublime-night-474607-f8', // same as your GCP project ID
-  keyFilename: './gcs-key.json', // path to your service account key JSON file
+  keyFilename: path.join(__dirname, '../gcs-key.json'), // path to your service account key JSON file
 });
 
 console.log("GCS Client initialized with project ID:", storage.projectId);
